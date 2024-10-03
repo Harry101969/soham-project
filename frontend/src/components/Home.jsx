963dc6c4416333f6d6bfd30ede9fae9ffaeb9843
Harry101969 <harshagarwal19696@gmail.com>
2024-10-03 19:57:01 +0530

    Updated

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    // Define and immediately invoke the async function
+    async function getToken() {
+      const token = await localStorage.getItem("token"); // Corrected the key name
+      console.log(token);
+      if(!token){
+       navigate("/login")
+      }
+    }
+    getToken();
+  }, []); 
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
