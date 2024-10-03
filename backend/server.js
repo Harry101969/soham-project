9ac7713335b9e3e6eb525a932100d9f8aadc272c
Harry101969 <harshagarwal19696@gmail.com>
2024-10-03 19:47:45 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,7 @@ app.use(
 app.use(express.json()); // Parse JSON
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.log("Failed to connect to MongoDB", err));
 
